3147c9a7335a0ae8a4ac26b0e9a7fdbf7669384e
Abraham de la Torre <abraham@lyrasuite.io>
2025-10-29 11:55:26 -0600

    fix: templates.contract usa contextFactory y quita import LYRA_API_URL

--- src/lyra/templates/contract.js
+++ src/lyra/templates/contract.js
@@ -1,9 +1,22 @@
 // src/lyra/templates/contract.js
-import { get } from "../../core/http/client.js";
-import { LYRA_API_URL } from "../../config/index.js";
 
-export async function getContract(templateId, token) {
-  if (!templateId) throw new Error("templateId es requerido");
-  const url = `${LYRA_API_URL}/templates/${encodeURIComponent(templateId)}/contract`;
-  return await get(url, { token });
+// Tool: templates.contract
+// Usa el http del contextFactory (con token del request) y NO importa la URL directo.
+export default function templatesContract(contextFactory) {
+  // Devolvemos la función que ejecuta el tool
+  return async function run(input = {}) {
+    const { http } = contextFactory(); // http ya viene con baseURL + token
+    const { templateId } = input;
+
+    if (!templateId) {
+      const err = new Error('templateId requerido');
+      err.status = 400;
+      throw err;
+    }
+
+    // Llama al endpoint de contract del API de Lyra
+    const { data } = await http.get(`/templates/${templateId}/contract`);
+    // Puedes normalizar si quieres, por ahora devolvemos tal cual
+    return data;
+  };
 }
--- src/lyra/templates/index.js
+++ src/lyra/templates/index.js
@@ -2,18 +2,16 @@
 import { registerTool } from '../../core/nlu/intentRouter.js';
 import templatesCreate from './create.js';
 import templatesList from './list.js';
-// cuando lo agregues:
 import templatesContract from './contract.js';
 
-// Registramos EN ESTE ORDEN (como pactamos):
+// Registramos EN ESTE ORDEN
 export function registerTemplateTools(contextFactory) {
-  registerTool('templates.create',   () => templatesCreate(contextFactory()));
-  registerTool('templates.list',     () => templatesList(contextFactory()));
-  registerTool('templates.contract', () => templatesContract(contextFactory()));
-  // Próximas (cuando toque):
+  registerTool('templates.create',   () => templatesCreate(contextFactory));
+  registerTool('templates.list',     () => templatesList(contextFactory));
+  registerTool('templates.contract', () => templatesContract(contextFactory));
+  // Próximas:
   // registerTool('templates.listByAudience', ...);
   // registerTool('templates.getRaw', ...);
   // registerTool('templates.getPreview', ...);
   // registerTool('templates.getOne', ...);
-  // registerTool('templates.getContract', ...);
 }
