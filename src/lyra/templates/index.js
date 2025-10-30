4c1199143ef6403375fde05a0635697b334308ea
Abraham de la Torre <abraham@lyrasuite.io>
2025-10-29 18:38:01 -0600

    cambio en como se importa el contract para mantener sesion

--- src/lyra/templates/index.js
+++ src/lyra/templates/index.js
@@ -2,16 +2,23 @@
 import { registerTool } from '../../core/nlu/intentRouter.js';
 import templatesCreate from './create.js';
 import templatesList from './list.js';
-import templatesContract from './contract.js';
+// ⬇️ importa también el registrador del contrato
+import { registerTemplateTools as registerContractTool } from './contract.js';
 
-// ¡OJO! Invocar contextFactory() para pasar { http, session, ... }
+// ¡OJO! Invocar contextFactory() (o inyectada) en cada tool
 export function registerTemplateTools(contextFactory) {
-  registerTool('templates.create',   () => templatesCreate(contextFactory()));
-  registerTool('templates.list',     () => templatesList(contextFactory()));
-  registerTool('templates.contract', () => templatesContract(contextFactory()));
+  // Estas dos siguen igual
+  registerTool('templates.create', (inj) => templatesCreate((inj || contextFactory)()));
+  registerTool('templates.list',   (inj) => templatesList((inj || contextFactory)()));
+
+  // ⬇️ Delegamos el registro de 'templates.contract' al módulo contract.js
+  // para que ese registro sea el que PERSISTE selectedTemplateId / contracts / provided.
+  registerContractTool(contextFactory);
+}
+
   // Próximas:
   // registerTool('templates.listByAudience', ...);
   // registerTool('templates.getRaw', ...);
   // registerTool('templates.getPreview', ...);
   // registerTool('templates.getOne', ...);
-}
+
