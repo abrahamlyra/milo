3c836b897aeed30db3c14c9925578829421c19b5
Abraham de la Torre <abraham@lyrasuite.io>
2025-11-03 12:48:01 -0600

    import in index

--- src/lyra/user/index.js
+++ src/lyra/user/index.js
@@ -1,2 +1,12 @@
 // src/lyra/user/index.js
+import { registerBillingTools } from './billing.js';
+
 export { userMe } from "./me.js";
+
+/**
+ * Registro de herramientas del módulo user (mínimo necesario).
+ * Llama al registrador de billing (wizard de Activar facturación).
+ */
+export function registerUserTools(contextFactory) {
+  registerBillingTools(contextFactory);
+}
